3dc4439f19fbdf2f1a652872204f206fb3dcf3ae
Pedro Henrique <pedrohsantorio.firebase@gmail.com>
2025-11-23 19:02:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3743254227-37481",
+  "appId": "1:598316788654:web:a32f394555e46be392f63f",
+  "apiKey": "AIzaSyBPFBfySs6JrYbDvP7i8fLnqTLPmXsVMZI",
+  "authDomain": "studio-3743254227-37481.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "598316788654"
+};
